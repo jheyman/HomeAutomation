548c505b76ec17715fffef201e3dfe8adf0a7109
Julien Heyman <bidsomail at gmail.com>
2016-08-18 21:21:03 +0200

    minor refactoring, more traces

--- ZWayServer/automation/modules/EcoMode/index.js
+++ ZWayServer/automation/modules/EcoMode/index.js
@@ -26,6 +26,24 @@ _module = EcoMode;
 // --- Module instance initialized
 // ----------------------------------------------------------------------------
 
+function setBinaryDevice(id, instance, level) {
+	if (zway.devices[id] != null) {
+		console.log("EcoMode: setting dev ", id, ", instance ", instance, " to level " , level);
+		zway.devices[id].instances[instance].SwitchBinary.Set(level);
+	} else {
+		console.log("EcoMode: NULL object trying to set dev ", id, ", instance ", instance, " to level " , level);
+	}
+}
+
+function setMultiLevelDevice(id, instance, level) {
+	if (zway.devices[id] != null) {
+		console.log("EcoMode: setting dev ", id, ", instance ", instance, " to level " , level);
+		zway.devices[id].instances[instance].SwitchMultilevel.Set(level);
+	} else {
+		console.log("EcoMode: NULL object trying to set dev ", id, ", instance ", instance, " to level " , level);
+	}
+}
+
 EcoMode.prototype.init = function (config) {
     EcoMode.super_.prototype.init.call(this, config);
 
@@ -45,58 +63,59 @@ EcoMode.prototype.init = function (config) {
 			try {
 				// Turn on the LED (full blue) to show ongoing transition to ECO mode
 				//blue
-				zway.devices[24].instances[2].SwitchMultilevel.Set(99);			
+				setMultiLevelDevice(24,2,99)
+		
 				//red
-				zway.devices[24].instances[3].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(24,3,0)
+
 				// green	
-				zway.devices[24].instances[4].SwitchMultilevel.Set(0);
-				
+				setMultiLevelDevice(24,4,0)
 
 				//FIBARO plug #1
-				zway.devices[5].instances[0].SwitchBinary.Set(0);
+				setBinaryDevice(5,0,0)
 
 				// Halogene Salon
-				zway.devices[8].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(8,0,0)
 
 				// POPP plug (Pi radio sdb tbb)
-				//zway.devices[9].instances[0].SwitchBinary.Set(0);
-
+				//setBinaryDevice(9,0,0)
+				
 				// Fibaro plug TVRdC
-				zway.devices[10].instances[0].SwitchBinary.Set(0);
+				setBinaryDevice(10,0,0)
 
 				//FIBARO plug #2	
-				zway.devices[11].instances[0].SwitchBinary.Set(0);
+				setBinaryDevice(11,0,0)
 
 				// Lumière SdBTBB
-				zway.devices[14].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(14,0,0)
 
 				// Lumière Dressing	
-				zway.devices[16].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(16,0,0)
 
 				//FIBARO plug #3	
-				zway.devices[17].instances[0].SwitchBinary.Set(0);
+				setBinaryDevice(17,0,0)
 
 				// Lumiere chambre
-				zway.devices[18].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(18,0,0)
 
 				// Lumiere SdB GBB
-				zway.devices[19].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(19,0,0)
 
 				// Lumiere escalier
-				zway.devices[20].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(20,0,0)
 
 				// Lumiere Entree
-				zway.devices[21].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(21,0,0)
 
 				// Lumiere Couloir Haut
-				zway.devices[22].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(22,0,0)
 
 				// Lumiere Bureau
-				zway.devices[23].instances[0].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(23,0,0)
 
 				// Lumiere Garage
-				zway.devices[27].instances[0].SwitchMultilevel.Set(0);
-	
+				setMultiLevelDevice(27,0,0)
+
 			console.log("EcoMode is now ON ");
 			}
 			catch(e) {
@@ -115,23 +134,23 @@ EcoMode.prototype.init = function (config) {
 			try {
 				// Turn on the LED (full green) to show ongoing transition out of ECO mode
 				// green
-				zway.devices[24].instances[4].SwitchMultilevel.Set(99);			
+				setMultiLevelDevice(24,4,99)
 				// red			
-				zway.devices[24].instances[3].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(24,3,0)
 				// blue
-				zway.devices[24].instances[2].SwitchMultilevel.Set(0);
+				setMultiLevelDevice(24,2,0)
 
 				//FIBARO plug #1
-				//zway.devices[5].instances[0].SwitchBinary.Set(255);
+				//setBinaryDevice(5,0,255)
 
 				// POPP plug (Pi radio sdb tbb)
-				//zway.devices[9].instances[0].SwitchBinary.Set(255);
+				setBinaryDevice(9,0,255)
 
 				// Fibaro plug TVRdC
-				zway.devices[10].instances[0].SwitchBinary.Set(255);
+				setBinaryDevice(10,0,255)
 
 				// Lumiere Entree
-				zway.devices[21].instances[0].SwitchMultilevel.Set(255);
+				setMultiLevelDevice(21,0,255)
 
 				console.log("EcoMode is now OFF");
 			}
